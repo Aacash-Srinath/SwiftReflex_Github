8f2e80d92d8324673a9b05771306eb205b18933d
Allen Bijo <allenbijot@gmail.com>
2024-11-08 23:58:07 +0530

    proto1

---
+++ Assets/Scripts/Fallen/dropper.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
+
+public class CylinderDropper : MonoBehaviour
+{
+    public float minDelay = 1.0f;
+    public float maxDelay = 10.0f;
+    private Rigidbody rb;
+    private Renderer cylinderRenderer;
+    private XRGrabInteractable grabInteractable;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cylinderRenderer = GetComponent<Renderer>();
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+        // Add listener for select event to change color
+        grabInteractable.selectEntered.AddListener(ChangeColor);
+
+        StartCoroutine(DropCylinder());
+    }
+
+    private IEnumerator DropCylinder()
+    {
+        yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
+        rb.useGravity = true;
+    }
+
+    private void ChangeColor(SelectEnterEventArgs args)
+    {
+        cylinderRenderer.material.color = GetRandomColor();
+    }
+
+    private Color GetRandomColor()
+    {
+        return new Color(Random.value, Random.value, Random.value);
+    }
+}
