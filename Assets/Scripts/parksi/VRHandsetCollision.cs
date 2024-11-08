e01fe688556fe047f7a6ba6e0240c455c8616347
SiddhiMarri <siddhi.marri@gmail.com>
2024-11-09 00:48:33 +0530

    edit num 5

--- Assets/Scripts/parksi/VRHandsetCollision.cs
+++ Assets/Scripts/parksi/VRHandsetCollision.cs
@@ -5,10 +5,10 @@ public class VRHandsetCollision : MonoBehaviour
 {
     public TextMeshProUGUI textDisplay;  // Assign this in the Inspector
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter(Collision collision)
     {
         // Check if the object colliding is the VR Handset (tag it as "VRHandset")
-        if (other.gameObject.CompareTag("VRHandset"))
+        if (collision.gameObject.CompareTag("VRHandset"))
         {
             textDisplay.text = "Collided";
         }
