0441ec1fd9cd489bd9762cb708dc83157fbeae6e
SiddhiMarri <siddhi.marri@gmail.com>
2024-11-14 15:52:18 +0530

    finalworkingmodel

--- Assets/Scripts/parksi/SaberCollision.cs
+++ Assets/Scripts/parksi/SaberCollision.cs
@@ -8,20 +8,24 @@ public class SaberCollision : MonoBehaviour
     public TextMeshProUGUI collisionText; // Reference to the TextMeshPro text field
     private int check=0;
     private int levelcheck = 0;
-
-    private void OnTriggerEnter(Collider other)
+    void Start()
     {
-        if (other.CompareTag("Image"))
+        levelcheck = 0;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag=="Image")
         {
             collisionText.text = "GAME OVER!";
             StartCoroutine(SwitchSceneAfterDelay("gameover", 0.5f));
         }
-        else if (other.CompareTag("Middle Object"))
+        else if (other.gameObject.tag == "Middle Object")
         {
             collisionText.text = "Mid point reached";
             check = 1;
         }
-        else if (other.CompareTag("End"))
+        else if (other.gameObject.tag == "End")
         {
             if (check!=1)
             {
@@ -30,17 +34,20 @@ public class SaberCollision : MonoBehaviour
             else
             {
                 collisionText.text = "Yay! You have successfully completed the path";
-                if (levelcheck == 1)
+                if (levelcheck == 0)
                 {
+                    levelcheck = 1;
+
                     StartCoroutine(SwitchSceneAfterDelay("Level2", 0.5f));
-                    levelcheck++;
+                    
                 }
-                if (levelcheck == 1)
+                else if (levelcheck == 1)
                 {
+                    levelcheck = 2;
                     StartCoroutine(SwitchSceneAfterDelay("Level3", 0.5f));
-                    levelcheck++;
+                    
                 }
-                if (levelcheck == 2)
+                else if (levelcheck == 2)
                 {
                     StartCoroutine(SwitchSceneAfterDelay("TheEnd", 0.5f));
                 }
